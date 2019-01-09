df4857ebd424c158662c7c83c150085e771e7253
CHESHIRE <ag.harribey@gmail.com>
2019-01-09 14:52:34 +0100

    [N|origin@ah-add-setterparser] git init.

--- setter.go
+++ setter.go
@@ -38,7 +38,11 @@ func (*defaultSetterFactory) buildParser(k reflect.Kind) parser {
 	case reflect.String:
 		return &stringParser{}
 	case reflect.Int, reflect.Int64:
-		return &intParser{transformer: intTransformers[k]}
+		return &int64Parser{transformer: intTransformers[k]}
+	case reflect.Int32:
+		return &int32Parser{transformer: intTransformers[k]}
+		//	case reflect.Struct:
+		//		return &structParser{transformer: structTransformers[k]}
 	case reflect.Bool:
 		return &boolParser{}
 	}
@@ -79,10 +83,10 @@ func (e *ErrNotBoolValue) Error() string {
 func (s *boolParser) parse(value string, ptr bool) (interface{}, error) {
 	var v bool
 
-	switch strings.TrimSpace(value) {
-	case "t", "1", "true":
+	switch strings.ToUpper(strings.TrimSpace(value)) {
+	case "T", "1", "TRUE":
 		v = true
-	case "f", "0", "false":
+	case "F", "0", "FALSE":
 	default:
 		return nil, &ErrNotBoolValue{value: value}
 	}
@@ -147,13 +151,21 @@ var intTransformers = map[reflect.Kind]intTransformer{
 
 		return v
 	},
+	reflect.Int32: func(v int64, ptr bool) interface{} {
+		if ptr {
+			x := int32(v)
+			return &x
+		}
+
+		return int32(v)
+	},
 }
 
-type intParser struct {
+type int64Parser struct {
 	transformer intTransformer
 }
 
-func (s *intParser) parse(value string, ptr bool) (interface{}, error) {
+func (s *int64Parser) parse(value string, ptr bool) (interface{}, error) {
 	var v, err = strconv.ParseInt(value, 10, 0)
 
 	if err != nil {
@@ -162,3 +174,33 @@ func (s *intParser) parse(value string, ptr bool) (interface{}, error) {
 
 	return s.transformer(v, ptr), nil
 }
+
+type int32Parser struct {
+	transformer intTransformer
+}
+
+func (s *int32Parser) parse(value string, ptr bool) (interface{}, error) {
+	var v, err = strconv.ParseInt(value, 10, 0)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return s.transformer(v, ptr), nil
+}
+
+//var structTransformers = map[reflect.Kind]structTransformer{
+//	reflect.Struct: func(v interface{}, ptr bool) interface{} {
+//		return v
+//	},
+//}
+//
+//type structTransformer func(interface{}, bool) interface{}
+//
+//type structParser struct {
+//	transformer structTransformer
+//}
+//
+//func (s *structParser) parse(value string, ptr bool) (interface{}, error) {
+//	return s.transformer(value, ptr), nil
+//}
